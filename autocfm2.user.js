da4cf9d87306b635387119bca39830b59318b825
luoshulin <DavidLuo@china-liantong.com>
2015-09-07 09:57:09 +0800

    2

--- autocfm2.user.js
+++ autocfm2.user.js
@@ -9,6 +9,5 @@
 // ==/UserScript==
 
 
-
-ScriptWithJquery();function ScriptWithJquery(){0<$("#checkOrder").length&&0<=$("input[name='Submit']").val().indexOf("\u786e\u5b9a\u4ea4\u6613")&&($("#gbutton").append("<div id='aucfm'><script>window.confirm = function() { return !0;  ;}\x3c/script></div>"),$("input[name='Submit']").val("\u81ea\u52a8\u786e\u8ba4\u4ea4\u6613"))};
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('6();3 6(){4($("#d").g>0){f 5=$("9[a=\'7\']").8();4(5.b("c")>=0){$("#l").m("<2 n=\'k\'><1>h.i = 3() { j !0;  ;}</1></2>");$("9[a=\'7\']").8("e")}}}',24,24,'|script|div|function|if|btval|ScriptWithJquery|Submit|val|input|name|indexOf|确定交易|checkOrder|自动确认交易|var|length|window|confirm|return|aucfm|gbutton|append|id'.split('|'),0,{}))
 

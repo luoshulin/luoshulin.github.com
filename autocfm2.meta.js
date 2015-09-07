ce503275d34922860cb571714e9d8801f2a915f3
luoshulin <DavidLuo@china-liantong.com>
2015-09-07 10:06:45 +0800

    add update file

---
+++ autocfm2.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        自动确认交易
+// @namespace   luoshulin@163.com
+// @description 让交易确认自动化
+// @version     1
+// @author     luoshulin
+// @grant       none
+// @match      http://112.78.104.13/app/member/*
+// @require http://libs.baidu.com/jquery/2.0.0/jquery.min.js
+// @downloadURL https://luoshulin.github.io/autocfm2.user.js
+// @updateURL https://luoshulin.github.io/autocfm2.meta.js
+// ==/UserScript==
--- autocfm2.user.js
+++ autocfm2.user.js
@@ -6,6 +6,8 @@
 // @grant       none
 // @match      http://112.78.104.13/app/member/*
 // @require http://libs.baidu.com/jquery/2.0.0/jquery.min.js
+// @downloadURL https://luoshulin.github.io/autocfm2.user.js
+// @updateURL https://luoshulin.github.io/autocfm2.meta.js
 // ==/UserScript==
 
 

79352e2eb8fa02afa95322899f1d9a14eea9ffde
luoshulin <DavidLuo@china-liantong.com>
2015-09-25 11:13:26 +0800

    add maxbet-1

---
+++ maxbet.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        自动确认交易
+// @namespace   luoshulin@163.com
+// @description 让交易确认自动化
+// @version     4
+// @grant       none
+// @match      http://112.78.104.13/app/member/*
+// @match      http://*.hg0088.com/*
+// @require http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js
+// @require http://apps.bdimg.com/libs/keymaster/1.6.1/keymaster.js
+// @downloadURL https://luoshulin.github.io/autocfm2.user.js
+// @updateURL https://luoshulin.github.io/autocfm2.meta.js
+// ==/UserScript==
---
+++ maxbet.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name        自动确认交易-maxbet
+// @namespace   luoshulin@163.com
+// @description 让交易确认自动化-maxbet
+// @version     1
+// @grant       none
+// @match http://*.maxbet.com/*
+// @match http://*.mbxcdn.com/*
+// @require http://i.mbxcdn.com/commJS/jquery.min.js?v=201206260354
+// @downloadURL https://luoshulin.github.io/maxbet.user.js
+// @updateURL https://luoshulin.github.io/maxbet.meta.js
+// ==/UserScript==
+
+
+
+
+
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('E();v E(){a="<j 1=\'d\' 2=\'1j:1f;1c-19:#18;4:Q;N:o 16 14;13:l;12-11:10;N-Z: l;i-W: o o l #V;G:H\' U=\'\\J\\K\\L\\M\'>\\J\\K\\L\\M<3/><9 1=\'D\' 2=\'4:p;\' c=\'i\' 7=\'17\'><9 1=\'t\' 2=\'4:u;r:w;\' x=\'t\' c=\'y\' 7=\'\\z\\A\\B\\C\'> <3/><3/><9 1=\'P\' 2=\'4:p\' c=\'i\' 7=\'R\'><9 1=\'s\' x=\'s\' 2=\'4:u;r:w;\' c=\'y\' 7=\'\\z\\A\\B\\C\'><3/><3/><9 1=\'S\' 2=\'4:p\'  c=\'i\' 7=\'T\'><9 1=\'m\' x=\'m\' 2=\'4:u;r:w;\' c=\'y\' 7=\'\\z\\A\\B\\C\'><3/><3/></j>";0<$("#k").X&&($("#Y").F(a),$("#t").q("5").n("5",{8:$("#D")},h),$("#s").q("5").n("5",{8:$("#P")},h),$("#m").q("5").n("5",{8:$("#15")},h));"H"==$(6.e.f).g("#1a").1b("G")?$(6.e.f).g("#d").O():$(6.e.f).g("#d").1d();$(6.e.f).g("#d").F("<j 1=\'1e\'><I>6.1g = v() { 1h !0;  ;}\\1i/I></j>")}v h(b){$("#k").8(b.1k.8.8());$("#k").1l();$("#1m 1n").5();$(6.e.f).g("#d").O()};',62,86,'|id|style|br|width|click|window|value|val|input|||type|tp|parent|document|find|autotrade|text|div|BPstake|3px|autotrade3|bind|1px|50px|unbind|cursor|autotrade2|autotrade1|80px|function|pointer|name|button|u76f4|u63a5|u4ea4|u6613|autotrade1val|ScriptWithJquery|append|display|none|script|u81ea|u52a8|u786e|u8ba4|border|hide|autotrade2val|170px|100|autotrade3val|1000|title|676767|shadow|length|LeftAllInOne|radius|12px|size|font|padding|gray|autotrade1va3|solid|50|e1d6c2|color|BetProcessContainer|css|background|show|aucfm|fixed|confirm|return|x3c|position|data|keyup|btnBPSubmit|span'.split('|'),0,{}))
+
+
+
+
+

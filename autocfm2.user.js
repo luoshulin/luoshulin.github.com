4eaba9ae94de13cde8bc7ce2ba8713a473e15b3e
davidluo <DavidLuo@china-liantong.com>
2017-11-21 15:09:03 +0800

    20171121

--- autocfm2.user.js
+++ autocfm2.user.js
@@ -15,7 +15,39 @@
 
 
 
-eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('1C();1r();9 1C(){N b=1.1W("f");b.j="m/Z";b.1G="#1L{1q:1p;24:25;26:2o;E:w 1e 13;1b:u;v:2l;17-Q:15;14:1u;E-1m: u;m-1k: w w u #1i;}";1.27("2i")[0].2m(b)}9 1r(){0<$("#B").H&&($("#B").1d("<g e=\'O\'><o>3.U = 9() { t !0;  ;}\\R/o></g>"),$("7[h=\'1F\']").A("6").z("6",9(){1z{3.4.1.16("l").f.n="C"}1f(b){1g.1h(b)}}),$("7[h=\'2r\']").A("6").z("6",9(){1z{3.4.1.16("l").f.n="C"}1f(b){1g.1h(b)}}),$("p.1D").1E(\'\\r\\x\\1H\\1J\\1K<7 h="D" 1M="1N" e="D" 1P="t 1Q()" Q="8" 1V="10" j="m">\'));$(3.4.1).5("#1x").1y("f").P("n")&&"C"==$(3.4.1).5("#1x").Z("n")&&0<$("#B").H?(a="<g e=\'l\' f=\'1q:1p;28-2d:#2f;v:2g;E:w 1e 13;1b:u;17-Q:15;14:1u;E-1m: u;m-1k: w w u #1i;\' 2h=\'\\K\\J\\y\\G\\18\\19\\1a\'>\\K\\J\\y\\G\\18\\19\\1a<k/><7 e=\'1c\' f=\'v:L\' j=\'m\' q=\'1I\'><7 e=\'S\' f=\'v:L\' h=\'S\' j=\'T\' q=\'\\M\\V\\r\\x\'> <k/><k/><7 e=\'1j\' f=\'v:L\' j=\'m\' q=\'1O\'><7 e=\'W\' h=\'W\' j=\'T\' q=\'\\M\\V\\r\\x\'><k/><k/><7 e=\'1l\' f=\'v:L\'  j=\'m\' q=\'1R\'><7 e=\'X\' h=\'X\' j=\'T\' q=\'\\M\\V\\r\\x\'><k/><k/></g>",0<$(3.4.1).5("#1T #l").H?$(3.4.1).5("#l").1U():($(3.4.1).5("#1n").1o(a),$(3.4.1).5("#1n").1o("<g e=\'O\'><o>3.U = 9(s){t !0;1X(s.P(\\"\\y\\1Y\\1Z\\20\\21\\22\\")>0){ t !0;}    23{    };}\\R/o></g>"),0<$("#B").H&&$("#B").1d("<g e=\'O\'><o>3.U = 9() { t !0;  ;}\\R/o></g>"),$(3.4.1).5("#S").A("6").z("6",{i:$(3.4.1).5("#1c")},I),$(3.4.1).5("#W").A("6").z("6",{i:$(3.4.1).5("#1j")},I),$(3.4.1).5("#X").A("6").z("6",{i:$(3.4.1).5("#1l")},I),$("7[h=\'Y\']").i("\\K\\J\\y\\G\\r\\x"))):$(3.4.1).5("#1s").1y("f").P("n")&&"C"!=$(3.4.1).5("#1s").Z("n")?$(3.4.1).5("#l").1t():$("7[h=\'Y\']").i("\\K\\J\\y\\G\\r\\x")}$(3.1).29(9(b){});9 I(b){$("#D").i(b.2a.i.i());$("#D").2b();$(3.4.1).5("#l").1t();$("7[h=\'Y\']").6()}9 2c(){$F="2e";$1v=1w($F);"\\11"==$1v?12($F,"\\2j"):12($F,"\\11");$("g.2k").6()}9 1w(b){N c;t(c=1.1A.2n(1B 2p("(^| )"+b+"=([^;]*)(;|$)")))?2q(c[2]):"\\11"}9 12(b,c){N d=1B 2s;d.2t(d.2u()+2v);1.1A=b+"="+2w(c)+";2x="+d.1S()};',62,158,'|document||window|parent|find|click|input||function|||||id|style|div|name|val|type|br|tp|text|display|script||value|u4ea4||return|3px|width|1px|u6613|u786e|bind|unbind|gbutton|none|gold|border|flagname|u8ba4|length|autotrade|u52a8|u81ea|80px|u76f4|var|aucfm|indexOf|size|x3c|autotrade1|button|confirm|u63a5|autotrade2|autotrade3|Submit|css||u5173|setCookie|gray|cursor|12px|getElementById|font|u5df2|u751f|u6548|padding|autotrade1val|append|solid|catch|console|log|676767|autotrade2val|shadow|autotrade3val|radius|bet_div|after|fixed|position|ScriptWithJquery|info_div|hide|pointer|flag|getCookie|euro_open|attr|try|cookie|new|ImportCss|amount|html|FINISH|innerHTML|u91d1|50|u989d|uff1a|flagbtn|class|txt|100|onkeyup|CountWinGold|1000|toGMTString|order_div|show|maxlength|createElement|if|u5b9a|u8fdb|u884c|u4e0b|u6ce8|else|bottom|60px|right|getElementsByTagName|background|keydown|data|keyup|updatecookie|color|autotradeflag|e1d6c2|208px|title|head|u5f00|tiTimer|75px|appendChild|match|5px|RegExp|unescape|btnCancel|Date|setTime|getTime|2592E6|escape|expires'.split('|'),0,{}))
 
+ImportCss();
+ScriptWithJquery();
+function ImportCss() {
+    var b = document.createElement("style");
+    b.type = "text/css";
+    b.innerHTML = "#tp{position:absolute;background-color:#e1d6c2;width:208px;border:1px solid gray;padding:3px;font-size:12px;cursor:pointer;border-radius: 3px;text-shadow: 1px 1px 3px #676767;}";
+    document.getElementsByTagName("head")[0].appendChild(b);
+}
+function ScriptWithJquery() {
+    0 < $("#gbutton").length && $("#gbutton").append("<div id='aucfm'><script>window.confirm = function() { return !0;  ;}\x3c/script></div>");
+    if( 0 < $("#gbutton").length){
+            a = "<div id='tp' style='position:absolute;background-color:#e1d6c2;width:208px;border:1px solid gray;padding:3px;font-size:12px;cursor:pointer;border-radius: 3px;text-shadow: 1px 1px 3px #676767;' title='\u81ea\u52a8\u786e\u8ba4\u5df2\u751f\u6548'>\u81ea\u52a8\u786e\u8ba4\u5df2\u751f\u6548<br/><input id='autotrade1val' style='width:80px' type='text' value='50'><input id='autotrade1' style='width:80px' name='autotrade1' type='button' value='\u76f4\u63a5\u4ea4\u6613'> <br/><br/><input id='autotrade2val' style='width:80px' type='text' value='100'><input id='autotrade2' name='autotrade2' type='button' value='\u76f4\u63a5\u4ea4\u6613'><br/><br/><input id='autotrade3val' style='width:80px'  type='text' value='1000'><input id='autotrade3' name='autotrade3' type='button' value='\u76f4\u63a5\u4ea4\u6613'><br/><br/></div>";
+        if(  0 < $(window.parent.document).find("#order_div #tp").length)
+        {
+            $(window.parent.document).find("#tp").show() ;
+            $(window.parent.document).find("#autotrade1").unbind("click").bind("click", {val: $(window.parent.document).find("#autotrade1val") }, autotrade);
+            $(window.parent.document).find("#autotrade2").unbind("click").bind("click", {val: $(window.parent.document).find("#autotrade2val") }, autotrade);
+            $(window.parent.document).find("#autotrade3").unbind("click").bind("click", {val: $(window.parent.document).find("#autotrade3val") }, autotrade);
+            $("input[name='Submit']").val("\u81ea\u52a8\u786e\u8ba4\u4ea4\u6613");
+        }else{
+            $(window.parent.document).find("#bet_div").after(a);
+            $(window.parent.document).find("#bet_div").after("<div id='aucfm'><script>window.confirm = function(s){return !0;if(s.indexOf(\"\u786e\u5b9a\u8fdb\u884c\u4e0b\u6ce8\")>0){ return !0;}    else{    };}\x3c/script></div>");
+        }
+       }else{
+           $(window.parent.document).find("#tp").hide();
+       }
 
+}
+function autotrade(b) {
+    $("#gold").val(b.data.val.val());
+    $("#gold").keyup();
+    $(window.parent.document).find("#tp").hide();
+    $("input[name='Submit']").click();
+}
 

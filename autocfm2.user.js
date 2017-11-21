7043acce4ae6670bd254f333a4d8eab8ade3203a
davidluo <DavidLuo@china-liantong.com>
2017-11-21 15:46:04 +0800

    1

--- autocfm2.user.js
+++ autocfm2.user.js
@@ -4,7 +4,7 @@
 // @description 让交易确认自动化
 // @version     5
 // @grant       none
-// @match      http://66.133.*/app/member/*
+// @match      http://*/app/member/*
 // @match      http://*.hg0088.com/*
 // @require http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js
 // @require http://apps.bdimg.com/libs/keymaster/1.6.1/keymaster.js
